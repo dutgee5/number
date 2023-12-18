f2150cc89fed951957db56d4a414c9c4379e76d9
theDone <muzodone5@gmail.com>
2023-12-18 22:44:08 +0300

    the style of the home page has been changed

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { StyleSheet} from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgorundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgorundImage:{
+    opacity:0.15
+  }
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,58 @@
-import { TextInput,View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.inputConteiner}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputConteiner: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#3b021f",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black" /*IOS*/,
+    shadowOffset: { width: 0, height: 2 } /*IOS*/,
+    shadowRadius: 6 /*IOS*/,
+    shadowRadius: 0.25 /*IOS*/,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    width: 50,
+    textAlign: "center",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer:{
+    flex:1,
+  }
+});
